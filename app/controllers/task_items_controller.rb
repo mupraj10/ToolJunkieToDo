3ebc1d7fb0bdc78de3b294ecb51a5ea3fdbb03ea
Maggy Prajapati <mupraj10@gmail.com>
2018-04-21 18:30:26 -0400

    tasks can be marked as completed now;

--- app/controllers/task_items_controller.rb
+++ app/controllers/task_items_controller.rb
@@ -1,5 +1,7 @@
 class TaskItemsController < ApplicationController
   before_action :set_task_list
+  before_action :set_task_item, except: [:create]
+  #   need those to do before any action happens
 
   #   creating a new item under the task
   def create
@@ -17,12 +19,22 @@ class TaskItemsController < ApplicationController
     redirect_to @task_list
   end
 
+  def complete
+    @task_item.update_attribute(:completed_at, Time.now)
+    redirect_to @task_list, notice: "Item completed!!"
+  end
+
   #   private methods so we do before load I assume?
   private
 
   #   find the correct task list
   def set_task_list
     @task_list = TaskList.find(params[:task_list_id])
+  end
+
+  # find the correct task item
+  def set_task_item
+    @task_item = @task_list.task_items.find(params[:id])
   end
 
   # find the correct item
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # actions for other routes
   resources :task_lists do
     # bringing in the task items
-    resources :task_items
+    resources :task_items do
+      member do
+        patch :complete
+      end
+    end
   end
+
   resources :users
 
   # inital landing page route
